b745109e7078b09e8e885bebaffcbc2c136448f5
Jaimes Subroto <jaimessubroto@gmail.com>
2024-09-27 11:18:53 -0700

    Remove dependencies

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 var app = require('express')();
-var request = require('request');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.listen(process.env.PORT || 5000);
 
-app.post('/image-annotate', function(req, res) {
-    var image = req.body.imageContent;
-    request.post(
-        'https://vision.googleapis.com/v1/images:annotate?key=<api_key>', {
-            json: {
-                "requests": [{
-                    "image": {
-                        "content": image
-                    },
-                    "features": [{
-                        "type": "DOCUMENT_TEXT_DETECTION"
-                    }]
-                }]
-            }
+app.post('/image-annotate', async function(req, res) {
+    const image = req.body.imageContent;
+    const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=<api_key>', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
         },
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var code = body.responses[0].textAnnotations[0].description;
-                console.log(code);
-                res.status(200).json({data: code});
-            }
-        }
-    );
+        body: JSON.stringify({
+            "requests": [{
+                "image": {
+                    "content": image
+                },
+                "features": [{
+                    "type": "DOCUMENT_TEXT_DETECTION"
+                }]
+            }]
+        })
+    })
+    if (response.status !== 200) {
+        res.status(response.status).send("Error with API request")
+    }
+    const json = await response.json()
+    const code = json.responses[0].textAnnotations[0].description;
+    console.log(code);
+    res.status(200).json({ data: code });
 })
